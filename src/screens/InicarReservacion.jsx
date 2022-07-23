107ef633c4cd97014bf72380fd873324033910b9
José Pullaguari <jepullaguari@utpl.edu.ec>
2022-07-23 18:12:10 -0500

    Estructura fronted

---
+++ src/screens/InicarReservacion.jsx
@@ -0,0 +1,13 @@
+import react from "react";
+import { View, Text } from "react-native";
+
+const InicarReservacion= ()=>{
+    return(
+        <View>
+            <Text> Login Screen </Text>
+        </View>
+    )
+}
+
+
+export default InicarReservacion
